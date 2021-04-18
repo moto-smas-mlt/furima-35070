73be93e4256a6eb174e2a8cd4bdf5e71f39cef09
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-18 18:52:32 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {'test01'}
-    email{Faker::Internet.free_email}
+    nickname { 'test01' }
+    email { Faker::Internet.free_email }
     password = 'abc123'
-    password{password}
-    password_confirmation{password}
-    last_name{'てすと'}
-    first_name{'わん'}
-    last_name_kana{'テスト'}
-    first_name_kana{'ワン'}
-    birthday{'2000/1/1'}
+    password { password }
+    password_confirmation { password }
+    last_name { 'てすと' }
+    first_name { 'わん' }
+    last_name_kana { 'テスト' }
+    first_name_kana { 'ワン' }
+    birthday { '2000/1/1' }
   end
 end
