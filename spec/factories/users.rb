a96192ab15089d52793ddb00e13d46287d773c60
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-19 15:25:29 +0900

    単体テスト可読性アップの為

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname { 'test01' }
     email { Faker::Internet.free_email }
-    password = 'abc123'
-    password { password }
-    password_confirmation { password }
-    last_name { 'てすと' }
-    first_name { 'わん' }
-    last_name_kana { 'テスト' }
-    first_name_kana { 'ワン' }
+    password  {'abc123'}
+    password_confirmation {password}
+    last_name { '赤あおキイロ' }
+    first_name { '赤あおキイロ' }
+    last_name_kana { 'アカアオキイロ' }
+    first_name_kana { 'アカアオキイロ' }
     birthday { '2000/1/1' }
   end
 end
