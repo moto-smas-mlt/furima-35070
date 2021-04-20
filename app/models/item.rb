9a3cc3ebf887646d0d8b6a3466a83ffad6f9c683
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-20 12:41:02 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_charge_burden
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
   has_one_attached :image
 end
