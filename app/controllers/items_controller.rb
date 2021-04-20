32528d2334fdecc4e8c5e73da574609e8610b50a
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-20 17:01:30 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+  def new
+  end
 
   private
-
   def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
