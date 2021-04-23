3538c6dfd127b944431c3e123cbe82434b327232
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-23 19:35:55 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :login_difference, only: [:edit,:update]
+  before_action :login_difference, only: [:edit, :update]
 
   def new
     @item = Item.new
