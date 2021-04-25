cace2f5836456ccb77dbaa20c0a96e1623c0ff81
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-25 17:56:41 +0900

    Formオブジェクト機能実装

---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality
+    validates :house_number
+    validates :tel_number
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    buy_item = BuyItem.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, tel_number: tel_number, buy_item_id: buy_item.id)
+  end
+end
