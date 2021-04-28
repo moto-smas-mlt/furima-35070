ec62c0837186254c2e9e5fb180249735d173482e
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-28 09:28:17 +0900

    商品購入アクション更新

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,20 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = Order.new
+    redirect_to root_path if @item.user_id == current_user.id
+    redirect_to root_path if @item.buy_item.present?
   end
 
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -20,6 +22,21 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:use_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).permit(:item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :tel_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
