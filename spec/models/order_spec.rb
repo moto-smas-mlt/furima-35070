ef9ac0c2f90389b296368049ce72ad27445314cf
moto-smas-mlt <keisukeoono.dr.pearl@gmail.com>
2021-04-28 12:40:21 +0900

    単体テストコード修正

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   before do
-    @order = FactoryBot.build(:order)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order = FactoryBot.build(:order, user_id: user.id, item_id: item.id)
+    sleep 2
   end
 
   describe '商品の購入情報が保存される' do
@@ -68,13 +71,13 @@ RSpec.describe Order, type: :model do
       end
 
       it 'user_idが空では登録できないこと' do
-        @order.user_id = ''
+        @order.user_id = nil
         @order.valid?
         expect(@order.errors.full_messages).to include("User can't be blank")
       end
 
       it 'item_idが空では登録できないこと' do
-        @order.item_id = ''
+        @order.item_id = nil
         @order.valid?
         expect(@order.errors.full_messages).to include("Item can't be blank")
       end
